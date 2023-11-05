cee2407e3be6167513475732692e4811d7cb1116
Totomomokazu <kanketomokazu@gmail.com>
2023-11-06 01:31:09 +0900

    課題作成中

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 window.onload = function(){
+    console.log("ウィンドウを読み込みました")
     let canvas = $("#canvas_test"); //canvasのidを取得
-    let ctx = canvas.getContext("2d"); //canvasが提供しているgetContext()というメソッドを利用。これを利用することでキャンバス上に図形やテキスト、画像などを描画できる ※getContext("2d")なので、今回の場合は2d描写
+    let ctx = canvas[0].getContext("2d"); //canvasが提供しているgetContext()というメソッドを利用。これを利用することでキャンバス上に図形やテキスト、画像などを描画できる ※getContext("2d")なので、今回の場合は2d描写
     let img=new Image(); //jsで画像データを扱うために利用するコード。js内で画像データを動的に扱うためのオブジェクトを作成
+    console.log("変数を定義しました")
+    // img.crossOrigin="anonymous";
+    
+    console.log("imgタグにsrcを指定します。")
+    img.src="img/1.adventure mood.png";
+    console.log("imgタグにsrcを指定しました。")
 
-
-
+    img.onload=function(){ //画像が正常に読み込まれたときに実行する関数
+        ctx.drawImage(img,0,0); //キャンバス上にimg変数に格納されている画像を描画。この画像はキャンバスの(0,0)の位置に描画される。
+        console.log("画像を描画しました")
+        window.localStorage["img"]=canvas[0].toDataURL();//キャンバスに描画された画像をimgというキーを使い、imgのデータURLを取得。
+        console.log("画像をlocalstorageに保存しました。")
+        // $("#canvas_test").attr("src",window.localStorage["img"]);    
+    }
 
 }
 
