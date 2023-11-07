ad759341304898a21480033f25ed704fa98b6aa4
Totomomokazu <kanketomokazu@gmail.com>
2023-11-07 23:24:54 +0900

    画像アップロード画面の完成

--- js/app2.js
+++ js/app2.js
@@ -35,21 +35,36 @@ $(document).ready(function(){
     //ドロップされたファイルの処理
     function filehandle(file){
         const reader = new FileReader();//ファイルAPIの呼び出し
+        console.log("ファイルAPIの読み出し")
         reader.readAsDataURL(file) //FileReaderのメソッドを呼び出し、fileの内容を読み込む.これが実行されたらonloadが開始される
+        console.log("ファイルの読み込み")
         reader.onload = function(e){ //ファイルの読み込みが完了した時に実行されるイベント
             preview.attr("src",e.target.result).show()
+            console.log("プレビュー表示")
             //attrでsrc 属性の値にe.target.resultを代入
             //target.resultにより読み込んだファイルの取得
             //show()でpreview領域がdisplay:noneであっても表示できる
             filedata=e.target.result //読み込んだファイルデータをfileDataという変数で保存
+            console.log("ファイルの一時保存")
         };
     }
     
     //postボタンを押されたときの処理
     $("#post").on("click",function(){
+
         if(filedata && comment.val()){
-            localStorage.setItem("img",filedata);
-            localStorage.setItem("comment",comment.val());
+            let obj ={} //画像とコメントを格納する用のobjectを作成
+            count ++ //アップロードされた回数をカウント。これがlocalstorageに入れるキーになる
+            console.log("postボタン押下後の変数定義に成功")
+
+            obj={
+                img:filedata,
+                comment:comment.val()
+            }
+            console.log("objへの代入に成功")
+       
+            localStorage.setItem(count.toString(),JSON.stringify(obj));
+            console.log("localstrageへの保存に成功")
         }else{
             alert("画像とコメントを入力してください。")
         }
